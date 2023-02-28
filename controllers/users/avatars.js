7eb8178cfe1478d8df970f1cdcd2df90b936e255
Maks Novakov <maksnovakov78@gmail.com>
2023-02-28 19:42:03 +0200

    fix

--- controllers/users/avatars.js
+++ controllers/users/avatars.js
@@ -7,11 +7,13 @@ const avatarDir = path.join(__dirname, "../../", "public", "avatars");
 const avatars = async (req, res) => {
   const { _id: id } = req.user;
   const { originalname, path: tempUpload } = req.file;
-  const [extension] = originalname.spplit(".").reverse();
+  const [extension] = originalname.split(".").reverse();
   const fileName = `${id}.${extension}`;
   const resultUpload = path.join(avatarDir, fileName);
+
   await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", fileName);
+
+  const avatarURL = path.join(`/avatars/${fileName}`);
   await User.findByIdAndUpdate(id, { avatarURL });
   res.json({
     avatarURL,
